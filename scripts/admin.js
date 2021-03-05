12a61875a07a541707de170c0df14a87a41fabe0
Ryan Wise <rwise1999@yahoo.com>
2021-03-04 16:40:03 -0800

    yep

---
+++ scripts/admin.js
@@ -0,0 +1,10 @@
+function updateInventory() {
+	var A1New = document.getElementbyId("inventory").elements.namedItem("A1").value;
+	var B1New = document.getElementbyId("inventory").elements.namedItem("B1").value;
+	var B1ANew = document.getElementbyId("inventory").elements.namedItem("B1A").value;
+	var C1New = document.getElementbyId("inventory").elements.namedItem("C1").value;
+	var C2New = document.getElementbyId("inventory").elements.namedItem("C2").value;
+	
+
+}
+
