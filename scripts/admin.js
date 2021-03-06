e59cdf9951aa6000e90771f3d712b5bf02d37841
Ryan Wise <rwise1999@yahoo.com>
2021-03-05 16:40:05 -0800

    yup

--- scripts/admin.js
+++ scripts/admin.js
@@ -1,13 +1,20 @@
 function updateInventory() {
-	var A1New = document.getElementbyId("inventory").elements.namedItem("A1").value;
-	var B1New = document.getElementbyId("inventory").elements.namedItem("B1").value;
-	var B1ANew = document.getElementbyId("inventory").elements.namedItem("B1A").value;
-	var C1New = document.getElementbyId("inventory").elements.namedItem("C1").value;
-	var C2New = document.getElementbyId("inventory").elements.namedItem("C2").value;
-	var A1 = document.getElementbyId("inventory").elements.namedItem("A1").value;
-	var B1 = document.getElementbyId("inventory").elements.namedItem("B1").value;
-	var B1A = document.getElementbyId("inventory").elements.namedItem("B1A").value;
-	var C1 = document.getElementbyId("inventory").elements.namedItem("C1").value;
-	var C2 = document.getElementbyId("inventory").elements.namedItem("C2").value;
+	var A1New = document.getElementById("inventory").elements.namedItem("A1").value;
+	var B1New = document.getElementById("inventory").elements.namedItem("B1").value;
+	var B1ANew = document.getElementById("inventory").elements.namedItem("B1A").value;
+	var C1New = document.getElementById("inventory").elements.namedItem("C1").value;
+	var C2New = document.getElementById("inventory").elements.namedItem("C2").value;
+	var A1 = document.getElementById("A1");
+	var B1 = document.getElementById("B1");
+	var B1A = document.getElementById("B1A");
+	var C1 = document.getElementById("C1");
+	var C2 = document.getElementById("C2");
+
+	A1.innerHTML = A1New.toString();
+	B1.innerHTML = B1New.toString();
+	B1A.innerHTML = B1ANew.toString();
+	C1.innerHTML = C1New.toString();
+	C2.innerHTML = C2New.toString();
+
 }
 
