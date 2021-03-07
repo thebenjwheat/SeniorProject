bd8865d9669738acfc6b73ffe1b9880c40b7182e
Ryan Wise <rwise1999@yahoo.com>
2021-03-06 21:24:30 -0800

    maybe json will work for admin

--- scripts/admin.js
+++ scripts/admin.js
@@ -1,20 +1,33 @@
+function loadJSON(callback) {   
+
+    var xobj = new XMLHttpRequest();
+        xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'my_data.json', true); // Replace 'my_data' with the path to your file
+    xobj.onreadystatechange = function () {
+          if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+          }
+    };
+    xobj.send(null);  
+ }
+
 function updateInventory() {
 	var A1New = document.getElementById("inventory").elements.namedItem("A1").value;
 	var B1New = document.getElementById("inventory").elements.namedItem("B1").value;
 	var B1ANew = document.getElementById("inventory").elements.namedItem("B1A").value;
 	var C1New = document.getElementById("inventory").elements.namedItem("C1").value;
 	var C2New = document.getElementById("inventory").elements.namedItem("C2").value;
+	
 	var A1 = document.getElementById("A1");
 	var B1 = document.getElementById("B1");
 	var B1A = document.getElementById("B1A");
 	var C1 = document.getElementById("C1");
 	var C2 = document.getElementById("C2");
 
-	A1.innerHTML = A1New.toString();
-	B1.innerHTML = B1New.toString();
-	B1A.innerHTML = B1ANew.toString();
-	C1.innerHTML = C1New.toString();
-	C2.innerHTML = C2New.toString();
-
+	loadJSON(function(response) {
+    var actual_JSON = JSON.parse(response);
+ });
+	console.log(actual_JSON);
 }
 
