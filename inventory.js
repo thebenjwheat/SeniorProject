1621d072f270766c74370149aab9dbc4c3d45dbf
Ryan Wise <rwise1999@yahoo.com>
2021-03-07 19:02:23 -0800

    let's hope this works

--- inventory.js
+++ inventory.js
@@ -1,33 +1,24 @@
+function loadJSON(callback) {   
 
-        let inventoryData = [
-            { name: 'A1', available: 4},
-            { name: 'B1', available: 5},
-            { name: 'B1A', available: 5},
-            { name: 'C1', available: 5},
-            { name: 'C2', available: 5},                                 
-        ];
-
-        window.onload = () => {
-        loadTableData(inventoryData);
-        };
-
-        function loadTableData(inventoryData) {
-            const tableBody = document.getElementById('tableData');
-            let dataHtml = '';
-
-            for(let inventory of inventoryData) {
-                dataHtml += `<tr><td>${inventory.name}</td><td>${inventory.available}</td></tr>`;
-            }
-            console.log(dataHtml)
-
-            tableBody.innerHTML = dataHtml;
-        };
-
-        function saveData() {
-            const name = document.getElementById('inventory.name');
-            const room = document.getElementById('inventory.available');
-            console.log(name, room)
-        }
-
-
-
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', './scripts/my_data.json', true); // Replace 'my_data' with the path to your file
+    xobj.onreadystatechange = function() {
+          if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+          }
+    };
+    xobj.send(null);  
+ }
+ function init(){
+     loadJSON(function(response){
+        actual_JSON = JSON.parse(response);
+        document.getElementById("A1").innerHTML = actual_JSON.A1;
+        document.getElementById("B1").innerHTML = actual_JSON.B1;
+        document.getElementById("B1A").innerHTML = actual_JSON.B1A;
+        document.getElementById("C1").innerHTML = actual_JSON.C1;
+        document.getElementById("C2").innerHTML = actual_JSON.C2;
+     });
+}
+window.onload = init;
