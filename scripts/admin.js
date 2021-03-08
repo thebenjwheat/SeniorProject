43286717a5047f64b201eb2ada916b3bb2fe62b2
Ryan Wise <rwise1999@yahoo.com>
2021-03-07 23:48:08 -0800

    huh

--- scripts/admin.js
+++ scripts/admin.js
@@ -1,16 +1,22 @@
-function loadJSON(callback) {   
-
-    var xobj = new XMLHttpRequest();
-    xobj.overrideMimeType("application/json");
-    xobj.open('GET', './scripts/my_data.json', true); // Replace 'my_data' with the path to your file
-    xobj.onreadystatechange = function() {
-          if (xobj.readyState == 4 && xobj.status == "200") {
-            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-            callback(xobj.responseText);
-          }
+var xobj = new XMLHttpRequest();
+xobj.overrideMimeType("application/json");
+xobj.open('GET', '/scripts/my_data.json', true); // Replace 'my_data' with the path to your file
+xobj.onreadystatechange = function() {
+	if (xobj.readyState == 4 && xobj.status == "200") {
+		var actual_JSON = JSON.parse(response);
+		var A1New = document.getElementById("inventory").elements.namedItem("A1").value;
+		var B1New = document.getElementById("inventory").elements.namedItem("B1").value;
+		var B1ANew = document.getElementById("inventory").elements.namedItem("B1A").value;
+		var C1New = document.getElementById("inventory").elements.namedItem("C1").value;
+		var C2New = document.getElementById("inventory").elements.namedItem("C2").value;
+        actual_JSON.A1 = A1New;
+	    actual_JSON.B1 = B1New;
+	    actual_JSON.B1A = B1ANew;
+	    actual_JSON.C1 = C1New;
+	    actual_JSON.C2 = C2New;  
+    	}
     };
-    xobj.send(null);  
- }
+xobj.send(null);  
  function sendJSON(jsonObj) {
  	var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance 
  	xmlhttp.open("POST", './scripts/my_data.json');
